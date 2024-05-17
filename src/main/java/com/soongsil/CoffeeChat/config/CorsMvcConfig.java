9781dd19dfb2320b4d2a2bffbe91ea970fef018d
oxdjww <oxdjww@gmail.com>
2024-05-17 23:09:37 +0900

    fix: add OPTION to cors config

--- src/main/java/com/soongsil/CoffeeChat/config/CorsMvcConfig.java
+++ src/main/java/com/soongsil/CoffeeChat/config/CorsMvcConfig.java
@@ -8,10 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsMvcConfig implements WebMvcConfigurer { //컨트롤러에서 보내는 데이터를 받을수 있게끔
 	@Override
 	public void addCorsMappings(CorsRegistry corsRegistry) {
-
 		corsRegistry.addMapping("/**")  //모든 경로에서 매핑 진행
 			.exposedHeaders("Set-Cookie")      //노출할 헤더값은 쿠키헤더
-			.allowedOrigins("http://localhost:3000");  //리액트서버주소에서 허용
+			.allowedOrigins("http://localhost:3000")
+			.allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+			.allowedHeaders("*")
+			.allowCredentials(true)
+			.maxAge(3600);
 	}
-
 }
