6fa58ac7b6e48490dc332e5f21cff88ad7f8e584
oxdjww <oxdjww@gmail.com>
2024-05-17 23:10:58 +0900

    docs: add swagger default domain

--- src/main/java/com/soongsil/CoffeeChat/CoffeeChatApplication.java
+++ src/main/java/com/soongsil/CoffeeChat/CoffeeChatApplication.java
@@ -3,6 +3,15 @@ package com.soongsil.CoffeeChat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+	servers = {
+		@Server(url = "https://cogo.run", description = "Production server"),
+		@Server(url = "http://localhost:8080", description = "Local development server")
+	}
+)
 @SpringBootApplication
 public class CoffeeChatApplication {
 
